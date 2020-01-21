7446c70d400701bae0f32e200d935441feacc109
bentleybuilds <bentleybuilds@gmail.com>
2020-01-21 15:41:18 -0700

    Updates

---
+++ precache-manifest.42e30ea2ca9c5424b10f12086775896e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497291242a454a98588e73bf5308ddf7",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "ef87938a74d86df919b4",
+    "url": "/monstersRolodex/static/css/main.8e80e3a8.chunk.css"
+  },
+  {
+    "revision": "46b4a65202bc0a3437a7",
+    "url": "/monstersRolodex/static/js/2.0f0ce4e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monstersRolodex/static/js/2.0f0ce4e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef87938a74d86df919b4",
+    "url": "/monstersRolodex/static/js/main.8694fc42.chunk.js"
+  },
+  {
+    "revision": "118ec463c42502d2eb05",
+    "url": "/monstersRolodex/static/js/runtime-main.16e1ba02.js"
+  }
+]);
